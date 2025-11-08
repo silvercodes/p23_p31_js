7581d51abe2756f0dc6b61fd9e53f912629a2abb
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-11-08 15:08:09 +0300

    add: functions, LE, EC

--- 06.deep_functions/index.js
+++ 06.deep_functions/index.js
@@ -1,13 +1,100 @@
+'use strict';
+
+// var a = 10;
+// let b = 20;
+// console.log('🟢', a + b);
+
+// function func() {
+//     var a = 10;
+//     let b = 20;
+//     console.log('🟢', a + b);
+// }
+
+// func();
 
 
-var a = 10;
-let b = 20;
-console.log('🟢', a + b);
+//#region Global object
 
-function func() {
-    var a = 10;
-    let b = 20;
-    console.log('🟢', a + b);
+// console.log('🟢', window);
+// console.log('🟢', global);
+// console.log('🟢', globalThis);       // :-)
+
+
+// console.log('🟢', Math);
+// console.log('🟢', window);
+// console.log('🟢', window.Math);
+
+
+
+// var a = 'Mercury';
+// let b = 'Venus';
+
+// // console.log('🟢', window);
+// console.log('🟢', window.a);
+// console.log('🟢', window.b);
+
+
+// var a = 10;
+// window.b = 20;
+// console.log('🟢', window);
+
+// console.log('🟢', a);
+// console.log('🟢', b);
+
+
+// var a = 10;
+// window.b = 20;
+
+// delete window.a;        // ERROR
+// delete window.b;
+
+// console.log('🟢', window);
+
+
+// console.log('🟢', window);
+// var setTimeout = 'HOHOHOHO';
+// let setInterval = 'OKOKOKOK';
+// console.log('🟢', window);
+
+
+//#endregion
+
+
+// a = 20;
+// func();
+
+// var a;
+// function func() {
+//     console.log('🟢', 'func()');
+// }
+
+
+
+// a = 20;
+// func();
+
+// let a;
+// const func = function() {
+//     console.log('🟢', 'func()');
+// }
+
+
+
+
+
+const email = 'vasia@mail.com';
+
+function f1(name) {
+
+    function f2() {
+        console.log('🟢', `${name}: ${email}`);
+    }
+
+    f2();
 }
 
-func();
+f1('Vasia');
+
+
+
+
